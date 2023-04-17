36a5c808b04345d3e7e12e495f9bd09f95541776
Kavya Meka <44432239+KavyaReddy555@users.noreply.github.com>
2023-04-17 23:41:33 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
     "jsx-a11y/no-static-element-interactions": 0,
     "react/no-array-index-key": 0,
     "jsx-a11y/anchor-is-valid": [
-      "error",
+      "off",
       {
         components: ["Link"],
         specialLink: ["to", "hrefLeft", "hrefRight"],
